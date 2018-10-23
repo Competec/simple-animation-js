41ca58c008c31fb5427ba198be128ddfcd82c262
Raphael Hättich <11434@corp.competec.ch>
2018-10-23 18:18:00 +0200

    inital commit

---
+++ build.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+const fs = require('fs');
+const { compile } = require('google-closure-compiler-js');
+
+const fileIn = 'simpleAnimeJs.js';
+const fileOut = 'simpleAnimeJs.min.js';
+
+console.info('Compiling...');
+
+fs.unlink(fileOut, (error) => {
+  fs.readFile(fileIn, { encoding: 'utf-8' }, (error, data) => {
+    if (error) throw error;
+    const flags = {
+      jsCode: [{ src: data }],
+      languageIn: 'ES6',
+      languageOut: 'ES5',
+    };
+    const out = compile(flags);
+    fs.writeFile(fileOut, out.compiledCode, (error) => {
+      if (error) throw error;
+      console.info('successful 😄');
+    });
+  });
+});
+/* eslint-enable */
---
+++ simpleAnimeJs.js
@@ -0,0 +1,58 @@
+// Main function
+const animate = (options) => {
+    // Define Variables
+    const {
+        target,
+        animations,
+        defaultDuration = 250,
+        defaultEasing = 'linear',
+    } = options;
+    const transitions = [];
+    const styles = [];
+    // Helpers
+    const getDecimalFromPercentage = value => parseFloat(value) / 100;
+    const checkIfHeight = value => value.indexOf('height') !== -1;
+    const checkIfWidth = value => value.indexOf('width') !== -1;
+    const msToSeconds = value => `${value / 1000}s`;
+    // Handler
+    const handlePercentage = (domTarget, attribute, animateTo) => {
+        const checkedHeight = checkIfHeight(attribute);
+        const checkedWidth = checkIfWidth(attribute);
+        if (!checkedHeight && !checkedWidth) {
+            throw new Error('Invalid direction');
+        }
+        animateTo = animateTo !== 'auto' ? animateTo : '100%';
+        const value = checkedHeight
+            ? domTarget.scrollHeight
+            : domTarget.scrollWidth;
+        return `${value * getDecimalFromPercentage(animateTo)}px`;
+    };
+    // Loops
+    animations.forEach((animation) => {
+        const {
+            attribute,
+            isPercentage = false,
+            duration = defaultDuration,
+            easing = defaultEasing,
+        } = animation;
+        let { animateTo } = animation;
+
+        animateTo = isPercentage
+            ? handlePercentage(target, attribute, animateTo)
+            : animateTo;
+
+        styles.push({
+            attribute,
+            animateTo,
+        });
+        transitions.push(`${attribute} ${msToSeconds(duration)} ${easing}`);
+    });
+    // Append transition before attributes
+    target.style.transition = transitions.join();
+    // Append attributes after transitions
+    styles.forEach((style) => {
+        target.style[style.attribute] = style.animateTo;
+    });
+};
+
+module.exports = animate;
