695153f3a2f75815cb401650be6ec9efb2f22b3d
Raphael Hättich <raphael@haettich.ch>
2018-10-25 16:10:11 +0200

    formatting

--- build.js
+++ build.js
@@ -8,18 +8,18 @@ const fileOut = 'simpleAnimeJs.min.js';
 console.info('Compiling...');
 
 fs.unlink(fileOut, (error) => {
-  fs.readFile(fileIn, { encoding: 'utf-8' }, (error, data) => {
-    if (error) throw error;
-    const flags = {
-      jsCode: [{ src: data }],
-      languageIn: 'ES6',
-      languageOut: 'ES5',
-    };
-    const out = compile(flags);
-    fs.writeFile(fileOut, out.compiledCode, (error) => {
-      if (error) throw error;
-      console.info('successful 😄');
+    fs.readFile(fileIn, { encoding: 'utf-8' }, (error, data) => {
+        if (error) throw error;
+        const flags = {
+            jsCode: [{ src: data }],
+            languageIn: 'ES6',
+            languageOut: 'ES5',
+        };
+        const out = compile(flags);
+        fs.writeFile(fileOut, out.compiledCode, (error) => {
+            if (error) throw error;
+            console.info('successful 😄');
+        });
     });
-  });
 });
 /* eslint-enable */
