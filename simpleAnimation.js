e4f5a3a28cd4554c2c4bf74001853a6e8f60bf89
Raphael Hättich <11434@corp.competec.ch>
2018-10-31 13:09:30 +0100

    added jsdoc

--- simpleAnimation.js
+++ simpleAnimation.js
@@ -1,5 +1,73 @@
 'use strict';
-// Main function
+/**
+ @typedef options
+ @type {Object}
+ @property {DOMnode} target The DOM Node to animate.
+ @property {animation[]} animations An array of the properties that will be animated, see Animations. At least one property required.
+ @property {number=} [defaultDuration=250] The default animation duration.
+ @property {string=} [defaultEasing=linear] The default CSS easing type.
+ */
+/**
+ @typedef animation
+ @type {Object}
+ @property {string} attribute The CSS Attribute to animate.
+ @property {various} animateTo The value the element animates to.
+ @property {number=} [duration=defaultDuration] The default CSS easing type.
+ @property {string=} [easing=defaultEasing] The default CSS easing type.
+ @property {boolean=} [pctDimension=false] The default CSS easing type.
+ */
+/**
+ * Get a decimal value from a percentage string.
+ * @function
+ * @param {string} value - The percentage as string.
+ * @returns {number} the percentage as decimal number.
+ */
+const getDecimalFromPercentage = value => parseFloat(value) / 100;
+/**
+ * Check if the provided string contains height.
+ * @function
+ * @param {string} value - The string to test.
+ * @returns {boolean} the result as boolean.
+ */
+const checkIfHeight = value => value.indexOf('height') !== -1;
+/**
+ * Check if the provided string contains width.
+ * @function
+ * @param {string} value - The string to test.
+ * @returns {boolean} the result as boolean.
+ */
+const checkIfWidth = value => value.indexOf('width') !== -1;
+/**
+ * Convert miliseconds to seconds and add a 's' at the end.
+ * @function
+ * @param {number} value - The number to convert
+ * @returns {string} the result as string with a 's' at the end.
+ */
+const msToSeconds = value => `${value / 1000}s`;
+/**
+ * Converts the given percentage to pixel equvalent.
+ * @function
+ * @param {DOMnode} domTarget The DOM Node to get the height and width from.
+ * @param {string} attribute CSS attribute to check if it contains height or width.
+ * @param {string} animateTo The string with the end percentage.
+ * @returns {string} the result as string with 'px' added.
+ */
+const handlePercentage = (domTarget, attribute, animateTo) => {
+    const checkedHeight = checkIfHeight(attribute);
+    const checkedWidth = checkIfWidth(attribute);
+    if (!checkedHeight && !checkedWidth) {
+        throw new Error('Invalid direction');
+    }
+    const value = checkedHeight ?
+        domTarget.scrollHeight :
+        domTarget.scrollWidth;
+    return `${value * getDecimalFromPercentage(animateTo)}px`;
+};
+/**
+ * Entry point of the library
+ *
+ * @returns {options} The animation function
+ */
 const animate = (options) => {
     // Define Variables
     const {
@@ -10,24 +78,6 @@ const animate = (options) => {
     } = options;
     const transitions = [];
     const styles = [];
-    // Helpers
-    const getDecimalFromPercentage = value => parseFloat(value) / 100;
-    const checkIfHeight = value => value.indexOf('height') !== -1;
-    const checkIfWidth = value => value.indexOf('width') !== -1;
-    const msToSeconds = value => `${value / 1000}s`;
-    // Handler
-    const handlePercentage = (domTarget, attribute, animateTo) => {
-        const checkedHeight = checkIfHeight(attribute);
-        const checkedWidth = checkIfWidth(attribute);
-        if (!checkedHeight && !checkedWidth) {
-            throw new Error('Invalid direction');
-        }
-        animateTo = animateTo !== 'auto' ? animateTo : '100%';
-        const value = checkedHeight ?
-            domTarget.scrollHeight :
-            domTarget.scrollWidth;
-        return `${value * getDecimalFromPercentage(animateTo)}px`;
-    };
     // Loops
     animations.forEach((animation) => {
         const {
