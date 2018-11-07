0676eb8971dfeb3083d8b637ae922fd873d0d860
Raphael Hättich <11434@corp.competec.ch>
2018-11-07 15:57:06 +0100

    eslint

--- test/simpleAnimeJs.spec.js
+++ test/simpleAnimeJs.spec.js
@@ -1,11 +1,12 @@
+/* eslint-disable */
 require('./config');
 
 const simpleAnimeJs = require('../src/simpleAnimation');
 
-describe('simpleAnimeJs', function () {
-    describe('animations', function () {
+describe('simpleAnimeJs', function() {
+    describe('animations', function() {
         beforeEach(() => {
-            const template = document.createElement('div'); 
+            const template = document.createElement('div');
             template.id = 'testid';
             document.body.appendChild(template);
             this.options = {
@@ -15,45 +16,63 @@ describe('simpleAnimeJs', function () {
                         attribute: 'height',
                         pctToScroll: true,
                         animateTo: '100%',
-                        duration: 5000
+                        duration: 5000,
                     },
                     {
                         attribute: 'max-width',
                         pctToScroll: true,
                         animateTo: '100%',
-                        easing: 'fadeout'
+                        easing: 'fadeout',
                     },
                     {
                         attribute: 'opacity',
-                        animateTo: 1
-                    }
+                        animateTo: 1,
+                    },
                 ],
-            }
+            };
         });
         it('should set height and opacity variables', () => {
             simpleAnimeJs(this.options);
-            
+
             const element = document.getElementById('testid');
-            
-            expect(element.style._values).to.eql({"height": "0px", "opacity": "1", "max-width": "0px"});
-            expect(element.style.transition).to.eql('height 5s linear,max-width 0.25s fadeout,opacity 0.25s linear');
+
+            console.log('element.style._values', element.style._values);
+
+            expect(element.style._values).to.eql({
+                height: '0px',
+                opacity: '1',
+                'max-width': '0px',
+            });
+            expect(element.style.transition).to.eql(
+                'height 5s linear,max-width 0.25s fadeout,opacity 0.25s linear'
+            );
         });
         it('should throw an error if ispercentage and attribute false', () => {
             this.options.animations[0].attribute = 'high';
-            
-            assert.throws(simpleAnimeJs.bind(undefined, this.options), Error, "Invalid direction");
+
+            assert.throws(
+                simpleAnimeJs.bind(undefined, this.options),
+                Error,
+                'Invalid direction'
+            );
         });
         it('should use set default easing and default duration', () => {
             this.options.defaultEasing = 'fadein';
             this.options.defaultDuration = 1337;
             this.options.DEBUG = true;
-            
+
             simpleAnimeJs(this.options);
-            
+
             const element = document.getElementById('testid');
-            
-            expect(element.style._values).to.eql({"height": "0px", "opacity": "1", "max-width": "0px"});
-            expect(element.style.transition).to.eql('height 5s fadein,max-width 1.337s fadeout,opacity 1.337s fadein');
+
+            expect(element.style._values).to.eql({
+                height: '0px',
+                opacity: '1',
+                'max-width': '0px',
+            });
+            expect(element.style.transition).to.eql(
+                'height 5s fadein,max-width 1.337s fadeout,opacity 1.337s fadein'
+            );
         });
     });
 });
